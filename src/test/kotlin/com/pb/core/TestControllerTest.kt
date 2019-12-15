bf5e8b4ea144b291d5f303ce64491a3210fa71ac
Murat Slavich <Murat.Slavich@sidenis.com>
2019-12-16 00:05:27 +0700

    add docker-compose for creating db instance

---
+++ src/test/kotlin/com/pb/core/TestControllerTest.kt
@@ -0,0 +1,11 @@
+package com.pb.core
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+internal class TestControllerTest {
+
+}
